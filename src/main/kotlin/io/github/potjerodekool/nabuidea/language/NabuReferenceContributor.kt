0f184c1da280176778718a2a3407857b9db4defa
everttigchelaar@gmail.com <everttigchelaar@gmail.com>
2025-05-03 01:06:33 +0200

    changes

--- src/main/kotlin/io/github/potjerodekool/nabuidea/language/NabuReferenceContributor.kt
+++ src/main/kotlin/io/github/potjerodekool/nabuidea/language/NabuReferenceContributor.kt
@@ -1,17 +1,8 @@
 package io.github.potjerodekool.nabuidea.language
 
-import com.intellij.patterns.ElementPattern
-import com.intellij.patterns.ElementPatternCondition
-import com.intellij.patterns.IElementTypePattern
 import com.intellij.patterns.PlatformPatterns
-import com.intellij.patterns.PsiElementPattern
-import com.intellij.patterns.PsiExpressionPattern
-import com.intellij.patterns.TreeElementPattern
-import com.intellij.patterns.uast.injectionHostUExpression
-import com.intellij.platform.diagnostic.telemetry.PlatformMetrics
 import com.intellij.psi.*
 import com.intellij.util.ProcessingContext
-import org.jetbrains.uast.UElement
 
 class NabuReferenceContributor : PsiReferenceContributor() {
 
@@ -26,28 +17,9 @@ class NabuReferenceContributor : PsiReferenceContributor() {
             PlatformPatterns.psiElement(),
             referenceProvider
         )
-
-        // org.jetbrains.uast.UElement, com.intellij.util.ProcessingContext
-
-        /*
-            { element : UElement, context : ProcessingContext ->
-                return@registerUastReferenceProvider true
-            }
-         */
-
     }
 
 
 
-
-}
-
-class MyProvider : UastReferenceProvider() {
-    override fun getReferencesByElement(
-        element: UElement,
-        context: ProcessingContext
-    ): Array<PsiReference> {
-        return emptyArray()
-    }
 
 }
